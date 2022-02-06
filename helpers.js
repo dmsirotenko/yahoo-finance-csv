f403728a4f349b0d91599df346d111fdb5b57aa9
dmsirotenko <dmitriy.sirotencko@gmail.com>
2022-02-06 01:55:15 +0000

    feat: localized format of numbers

--- helpers.js
+++ helpers.js
@@ -3,11 +3,23 @@ const get = require('lodash/get');
 
 const sleep = util.promisify(setTimeout);
 
+function formatNumber(value, locale) {
+    return new Intl.NumberFormat(locale, { useGrouping: false }).format(value);
+}
+
+function formatValue(value, locale = 'ru-RU') {
+    if (typeof value === 'number') {
+        return formatNumber(value, locale)
+    }
+    
+    return value;
+}
+
 function mapColumnValues(columns, data = {}) {
     return columns.reduce((acc, { header, field, formatter }) => {
         const value = get(data, field);
 
-        acc[header] = formatter ? formatter(value) : value;
+        acc[header] = formatter ? formatter(value) : formatValue(value);
 
         return acc;
     }, {});
@@ -18,7 +30,7 @@ function extractEarnings(entries, key = 'earnings', separator = ',') {
         .map(({ date, ...values }) => {
             const value = values[key]?.raw ?? 0;
 
-            return `${date}:${value}`;
+            return `${date}:${formatValue(value)}`;
         })
         .join(separator);
 }
